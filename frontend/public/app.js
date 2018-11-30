97ff319199af6f5682c1c782f55c077af15a7f88
NicholasBallard <nicholasericballardgmail.com>
2018-11-30 15:06:59 -0500

    wiggle animation to ham menu!

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,92 +1,37 @@
-// #Auth0
+// Handle user authentication with Auth0
 
-window.addEventListener("load", function() {
-  var webAuth = new auth0.WebAuth({
-    domain: "amadealz.auth0.com",
-    clientID: "ghu2kM5kF5wb1qssWiltiUKZFv6Mvp3P",
-    responseType: "token id_token",
-    scope: "openid",
-    redirectUri: this.window.location.href
-  });
+window.addEventListener('load', function () {
+  var theWebAuth = new auth0.WebAuth({
+    domain: '',
+    clientID: '',
+    redirectUri: '',
+    responseType: 'token id_token',
+    scope: 'openid',
+    leeway: 60
+  })
 
-  var loginButton = document.querySelector(".button-login");
-
-  loginButton.addEventListener("click", function(e) {
-    e.preventDefault();
-    webAuth.authorize();
-  });
-
-//   Event handling
-
-  var loginStatus = document.querySelector(".container h4");
-  var loginView = document.getElementById("login-view");
-  var homeView = document.getElementById("home-view");
-
-  // buttons and event listeners
-  var homeViewBtn = document.getElementById("btn-home-view");
-  var loginBtn = document.getElementById("btn-login");
-  var logoutBtn = document.getElementById("btn-logout");
-
-  homeViewBtn.addEventListener("click", function() {
-    homeView.style.display = "inline-block";
-    loginView.style.display = "none";
-  });
-
-  logoutBtn.addEventListener("click", logout);
-
-  function handleAuthentication() {
-    webAuth.parseHash(function(err, authResult) {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        window.location.hash = "";
-        setSession(authResult);
-        loginBtn.style.display = "none";
-        homeView.style.display = "inline-block";
-      } else if (err) {
-        homeView.style.display = "inline-block";
-        console.log(err);
-        alert(
-          "Error: " + err.error + ". Check the console for further details."
-        );
-      }
-      displayButtons();
-    });
+  // DOM element variables for buttons
+  // and event listeners on click
+  
+  function setSession() {
+    
   }
-
-  function setSession(authResult) {
-    // Set the time that the Access Token will expire at
-    var expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
-    localStorage.setItem("access_token", authResult.accessToken);
-    localStorage.setItem("id_token", authResult.idToken);
-    localStorage.setItem("expires_at", expiresAt);
+  
+  function logOut() {
+    
   }
-
-  function logout() {
-    // Remove tokens and expiry time from localStorage
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
-    displayButtons();
-  }
-
+  
   function isAuthenticated() {
-    // Check whether the current time is past the
-    // Access Token's expiry time
-    var expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-    return new Date().getTime() < expiresAt;
+    
   }
 
-  function displayButtons() {
-    if (isAuthenticated()) {
-      loginBtn.style.display = "none";
-      logoutBtn.style.display = "inline-block";
-      loginStatus.innerHTML = "You are logged in!";
-    } else {
-      loginBtn.style.display = "inline-block";
-      logoutBtn.style.display = "none";
-      loginStatus.innerHTML =
-        "You are not logged in! Please log in to continue.";
-    }
+  function handleTheAuthentication() {
+    
   }
-});
+
+  function displayTheButtons() {
+
+  }
+
+  handleAuthentication()
+})
---
+++ frontend/public/auth0_variables.js
@@ -0,0 +1,3 @@
+const CLIENT_ID_AUTH0 = 'ghu2kM5kF5wb1qssWiltiUKZFv6Mvp3P'
+const DOMAIN_AUTH0 = 'amadealz.auth0.com'
+const CALLBACK_URL_AUTH0 = location.href
